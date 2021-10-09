f1d61f1e3ee81c44f6a2288977a2d5d8807f77bc
Adharv <91813192+NOISE3411@users.noreply.github.com>
2021-10-09 22:20:58 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,60 @@
+onEvent("welbutton", "click", function( ) {
+  setScreen("info");
+});
+onEvent("start", "click", function( ) {
+  setScreen("Q1");
+});
+onEvent("Q1a1", "click", function( ) {
+  setScreen("wrong1");
+});
+onEvent("Q1a2", "click", function( ) {
+  setScreen("Q1ans");
+});
+onEvent("Q1a3", "click", function( ) {
+  setScreen("wrong1");
+});
+onEvent("Q1a4", "click", function( ) {
+  setScreen("wrong1");
+});
+onEvent("wrong_button_1", "click", function( ) {
+  setScreen("Q1");
+});
+onEvent("next1", "click", function( ) {
+  setScreen("Q2");
+});
+onEvent("Q2a1", "click", function( ) {
+  setScreen("Q2ans");
+});
+onEvent("Q2a2", "click", function( ) {
+  setScreen("wrong2");
+});
+onEvent("Q2a3", "click", function( ) {
+  setScreen("wrong2");
+});
+onEvent("Q2a4", "click", function( ) {
+  setScreen("wrong2");
+});
+onEvent("next2", "click", function( ) {
+  setScreen("Q3");
+});
+onEvent("wrong_button_2", "click", function( ) {
+  setScreen("Q1");
+});
+onEvent("Q3a1", "click", function( ) {
+  setScreen("wrong3");
+});
+onEvent("Q3a2", "click", function( ) {
+  setScreen("wrong3");
+});
+onEvent("Q3a3", "click", function( ) {
+  setScreen("wrong3");
+});
+onEvent("Q3a4", "click", function( ) {
+  setScreen("Q3ans");
+});
+onEvent("restart", "click", function( ) {
+  setScreen("info");
+});
+onEvent("wrong_button_3", "click", function( ) {
+  setScreen("info");
+});
